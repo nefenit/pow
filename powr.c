d21979320248c200ebb97ac1c73a207dd1f0a66f
nefenit <nefenit@gmail.com>
2019-03-12 17:11:35 +0100

    Update powr.c

--- powr.c
+++ powr.c
@@ -3,6 +3,7 @@
  */
  
 #include <stdio.h>
+#include <stdlib.h>
  
 typedef unsigned long long ull;
 typedef long double ld;
@@ -16,6 +17,8 @@ ld powr(ld x, ull n)
 	return n ? x * powr(x, n-1) : 1;
  
 int main(int argc, char **argv) {
-
+	if(argc != 3)
+		usage(EXIT_FAILURE);
+	printf("%Lf\n", powr(strtold(argv[1], NULL), strtoull(argv[2], NULL, 10)));
 	return 0;
 }
